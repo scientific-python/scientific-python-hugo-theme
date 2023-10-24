864298186d096cd6f1da0edae5a85af04a1ad641
Jarrod Millman <jarrod.millman@gmail.com>
2023-10-24 08:15:53 -0700

    New test

--- assets/js/dark.js
+++ assets/js/dark.js
@@ -1,11 +1,26 @@
-window.matchMedia("(prefers-color-scheme: dark)")
-	.addEventListener("change", ({ matches }) => {
-    if (matches) {
-      targetTheme = "dark";
-    } else {
-      targetTheme = "light";
-    }
-    document.documentElement.setAttribute("data-theme", targetTheme);
-    localStorage.setItem("theme", targetTheme);
-    localStorage.setItem("mode", targetTheme);
-});
+/*******************************************************************************
+ * Theme interaction
+ */
+
+var prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+/**
+ * set the the body theme to the one specified by the user browser
+ *
+ * @param {event} e
+ */
+function autoTheme(e) {
+  targetTheme = prefersDark.matches ? "dark" : "light";
+  document.documentElement.dataset.theme = targetTheme;
+  document.documentElement.setAttribute("data-theme", targetTheme);
+  localStorage.setItem("theme", targetTheme);
+  localStorage.setItem("mode", targetTheme);
+}
+
+
+targetTheme = prefersDark.matches ? "dark" : "light";
+document.documentElement.dataset.theme = targetTheme;
+document.documentElement.setAttribute("data-theme", targetTheme);
+localStorage.setItem("theme", targetTheme);
+localStorage.setItem("mode", targetTheme);
+prefersDark.onchange = autoTheme
