77aed60f0583c3fb055db7a3fac4b58aafebb50a
Jarrod Millman <jarrod.millman@gmail.com>
2023-10-23 23:50:26 -0700

    Add javascript dark toggle mode
    I initially tried to use the themeswitcher from:
    https://github.com/pydata/pydata-sphinx-theme/blob/main/src/pydata_sphinx_theme/assets/scripts/pydata-sphinx-theme.js

---
+++ assets/js/dark.js
@@ -0,0 +1,11 @@
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", ({ matches }) => {
+    if (matches) {
+      targetTheme = "dark";
+    } else {
+      targetTheme = "light";
+    }
+    document.documentElement.setAttribute("data-theme", targetTheme);
+    localStorage.setItem("theme", targetTheme);
+  });
