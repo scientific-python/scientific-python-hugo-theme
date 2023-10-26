f31744f57ca51522c46a338918a8fd362b247d6c
Jarrod Millman <jarrod.millman@gmail.com>
2023-10-26 09:44:04 -0700

    Remove unused fadeFrontPage (#330)

--- assets/js/fresh.js
+++ assets/js/fresh.js
@@ -1,11 +1,3 @@
-//Preloader
-$(window).on("load", function () {
-  // makes sure the whole site is loaded
-  $("#status").fadeOut(); // will first fade out the loading animation
-  $("#preloader").delay(350).fadeOut("slow"); // will fade out the white DIV that covers the website.
-  $("body").delay(350).css({ overflow: "visible" });
-});
-
 $(document).ready(function () {
   //Mobile menu toggle
   if ($(".navbar-burger").length) {
