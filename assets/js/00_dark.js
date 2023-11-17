debbfed3baa6397d94d4336f673b47f96573350d
Stefan van der Walt <stefanv@berkeley.edu>
2023-11-16 20:24:26 -0800

    Allow disabling dark mode in configuration

--- assets/js/00_dark.js
+++ assets/js/00_dark.js
@@ -9,10 +9,18 @@ var prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
  *
  * @param {event} e
  */
-function autoTheme(e) {
-  targetTheme = prefersDark.matches ? "dark" : "light";
-  document.documentElement.setAttribute("data-theme", targetTheme);
+function setTheme() {
+  /* One of auto, light, or dark, depending on what the site wants to support */
+  const themeScheme = document.documentElement.getAttribute("data-colorscheme");
+
+  const browserScheme = prefersDark.matches ? "dark" : "light";
+
+  // Use the color scheme from the configuration file, if set
+  document.documentElement.setAttribute(
+    "data-theme",
+    themeScheme == "auto" ? browserScheme : themeScheme,
+  );
 }
 
-autoTheme();
-prefersDark.onchange = autoTheme;
+setTheme();
+prefersDark.onchange = setTheme;
